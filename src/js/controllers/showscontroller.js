cd3ef0277073d3377b4e3e60b7b9363de126845f
ThankGod Richard <madueketf@gmail.com>
2021-10-15 00:43:24 +0100

    added test results

--- src/js/controllers/showscontroller.js
+++ src/js/controllers/showscontroller.js
@@ -17,11 +17,41 @@ export default class ShowController {
         .then((result) => {
           const movie = JSON.parse(result);
           this.moviesArray.push(movie);
+          this.printScreen(movie);
+        })
+        .catch((error) => {
+          throw new Error(error);
+        });
 
-          const divHolder = document.createElement('div');
-          divHolder.className = 'col-md-4 col-lg-4 mt-2';
-          if (movie.image) {
-            divHolder.innerHTML = `<div class="card">
+      start += 1;
+    }
+    new LikesControllers().getLikes();
+  };
+
+  sendLikes = (movieID, id) => {
+    const likeController = new LikesControllers();
+    const response = likeController.sendLike(movieID);
+    response
+      .then(() => {
+        const likeCount = document.getElementById(`${id}_count`);
+        const [currentLike, keep] = likeCount.innerHTML.split(' ');
+        const intVal = parseInt(currentLike, 10);
+        likeCount.innerHTML = `${intVal + 1} ${keep}`;
+      })
+      .catch((er) => er);
+  };
+
+  countShows() {
+    this.totatShows += 1;
+    const previousTotal = document.getElementById('totalShowsCount');
+    previousTotal.innerHTML = this.totatShows;
+  }
+
+  printScreen(movie) {
+    const divHolder = document.createElement('div');
+    divHolder.className = 'col-md-4 col-lg-4 mt-2';
+    if (movie.image) {
+      divHolder.innerHTML = `<div class="card">
           <div class="card-body">
             <div class="card-img-actions">
               <img
@@ -57,50 +87,23 @@ export default class ShowController {
             </button>
           </div>
         </div>`;
-          }
-          this.likesControllers.getLikes().then((res) => {
-            const obj = res.find((o) => o.item_id === movie.id);
-            if (obj != null) {
-              const likeCount = document.getElementById(`likes${movie.id}_count`);
-              const [, keep] = likeCount.innerHTML.split(' ');
-
-              const intVal = parseInt(obj.likes, 10);
-              likeCount.innerHTML = `${intVal} ${keep}`;
-            }
-          });
-
-          document.getElementById('cardHolder').appendChild(divHolder);
-          this.countShows();
-          const likeLiterner = document.getElementById(`likes${movie.id}`);
-          likeLiterner.addEventListener('click', () => {
-            this.sendLikes(movie.id, `likes${movie.id}`);
-          });
-        })
-        .catch((error) => {
-          throw new Error(error);
-        });
-
-      start += 1;
     }
-    new LikesControllers().getLikes();
-  };
+    this.likesControllers.getLikes().then((res) => {
+      const obj = res.find((o) => o.item_id === movie.id);
+      if (obj != null) {
+        const likeCount = document.getElementById(`likes${movie.id}_count`);
+        const [, keep] = likeCount.innerHTML.split(' ');
 
-  sendLikes = (movieID, id) => {
-    const likeController = new LikesControllers();
-    const response = likeController.sendLike(movieID);
-    response
-      .then(() => {
-        const likeCount = document.getElementById(`${id}_count`);
-        const [currentLike, keep] = likeCount.innerHTML.split(' ');
-        const intVal = parseInt(currentLike, 10);
-        likeCount.innerHTML = `${intVal + 1} ${keep}`;
-      })
-      .catch((er) => er);
-  };
+        const intVal = parseInt(obj.likes, 10);
+        likeCount.innerHTML = `${intVal} ${keep}`;
+      }
+    });
 
-  countShows() {
-    this.totatShows += 1;
-    const previousTotal = document.getElementById('totalShowsCount');
-    previousTotal.innerHTML = this.totatShows;
+    document.getElementById('cardHolder').appendChild(divHolder);
+    this.countShows();
+    const likeLiterner = document.getElementById(`likes${movie.id}`);
+    likeLiterner.addEventListener('click', () => {
+      this.sendLikes(movie.id, `likes${movie.id}`);
+    });
   }
 }
